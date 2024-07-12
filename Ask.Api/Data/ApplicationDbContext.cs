08ef655581143c778925f8a860dcb7c08e623bc1
abdalla-elmahdy <abdalla.m.elmahdy@gmail.com>
2024-07-13 00:29:32 +0300

    Initial structure and boilerplate identity features

---
+++ Ask.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ask.Api.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext<IdentityUser>(options)
+{
+
+}
